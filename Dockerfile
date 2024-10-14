01c12c2c77299a482f6290b4ad25261abf8125a1
boterop <boterop22@gmail.com>
2024-10-14 15:34:23 -0500

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM elixir:1.17.3
+
+WORKDIR /usr/src/app
+
+ENV MIX_ENV=dev
+
+COPY mix.exs ./
+
+RUN mix local.hex --force
+RUN mix local.rebar --force
+RUN mix deps.get
+
+COPY . .
+
+RUN mix do phx.digest, compile 
+
+EXPOSE 4000
+CMD ["mix", "phx.server"]
--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :phx_liveview, PhxLiveview.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "phx_liveview_dev",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASSWORD") || "postgres",
+  hostname: System.get_env("DB_HOST") || "localhost",
+  database: System.get_env("DB_NAME") || "phx_liveview_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +19,7 @@ config :phx_liveview, PhxLiveview.Repo,
 config :phx_liveview, PhxLiveviewWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
