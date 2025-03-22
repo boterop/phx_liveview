69ad1906261d1fb960df7c5b62486213b7725bf9
Santiago Botero <98826652+boterop@users.noreply.github.com>
2025-03-22 15:47:31 -0500

    Prepare to release v0.3.0 (#12)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 0.3.0 (22.03.2025)
+
+- Home as liveview
+- Run migrations on startup in releases
+- Run mix deps.get on startup
+- Setup script to rename after template
+
 ## 0.2.0 (01.03.2025)
 
 - Use localhost instead of docker host
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxLiveview.MixProject do
   def project do
     [
       app: :phx_liveview,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
